02216bb7a49e40c8f5a5327a46b4e7a6c03e1f4c
Angelo Lucas <Angelolucasfl@gmail.com>
2024-01-31 20:07:48 -0300

    Copy functionality

--- src/app/emoji/emoji.component.ts
+++ src/app/emoji/emoji.component.ts
@@ -8,7 +8,10 @@ import { Component, OnInit, inject } from '@angular/core';
   templateUrl: './emoji.component.html',
   styleUrl: './emoji.component.css'
 })
+
 export class EmojiComponent implements OnInit{
+  constructor() { }
+
   http = inject(HttpClient);
   emojis: any = [];
 
@@ -20,11 +23,33 @@ export class EmojiComponent implements OnInit{
     this.http.get("https://emojihub.yurace.pro/api/all/category/flags")
       .subscribe((emojis: any) => {
         console.log(emojis);
-        this.emojis = emojis;
-      })
+        this.emojis = emojis || [];
+      });
   }
 
   joinHtmlCodes(htmlCodes: string[]): string {
-    return htmlCodes.join('');
+    return htmlCodes.join("");
   }
+
+  copyEmoji(htmlCode: string[]): void {
+    try {
+      const emojiCharacter = this.convertHtmlToEmoji(this.joinHtmlCodes(htmlCode));
+
+      navigator.clipboard.writeText(emojiCharacter).then(() => {
+        console.log('Emoji copiado para a área de transferência:', emojiCharacter);
+      }).catch((error) => {
+        console.error('Erro ao copiar o emoji para a área de transferência:', error);
+      });
+    } catch (error) {
+      console.error('Erro ao copiar o emoji:', error);
+    }
+  }
+
+  convertHtmlToEmoji(htmlCode: string): string {
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = htmlCode;
+    const emojiCharacter = tempElement.textContent || '';
+    return emojiCharacter;
+  }
+
 }
