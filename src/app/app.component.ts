3718e5a425b59290896a73c68ae6d2154801fd61
Angelo Lucas <Angelolucasfl@gmail.com>
2024-01-30 22:00:54 -0300

    first commit"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmojiComponent } from './emoji/emoji.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmojiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/emoji/emoji.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-emoji',
+  standalone: true,
+  imports: [],
+  templateUrl: './emoji.component.html',
+  styleUrl: './emoji.component.css'
+})
+export class EmojiComponent implements OnInit{
+  http = inject(HttpClient);
+  emojis: any = [];
+
+  ngOnInit(): void {
+    this.fetchEmojis();
+  }
+
+  fetchEmojis(){
+    this.http.get("https://emojihub.yurace.pro/api/all/category/flags")
+    .subscribe((emojis: any) => {
+      console.log(emojis);
+      this.emojis = emojis;
+    })
+  }
+}
