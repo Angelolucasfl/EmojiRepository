05a59057e87267fcb605ba2c7e643b087cfdc382
Angelo Lucas <Angelolucasfl@gmail.com>
2024-02-04 21:37:22 -0300

    adding navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmojiComponent } from './emoji/emoji.component';
 import { FilterBarComponent } from './filter-bar/filter-bar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { Category } from './shared/types';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EmojiComponent, FilterBarComponent],
+  imports: [RouterOutlet, EmojiComponent, FilterBarComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
